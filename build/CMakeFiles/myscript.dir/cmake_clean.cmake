d6b0a93a5b54cf4bdd0d358a1ff6a8c100d82f55
Anoop Ramakrishna <revelationnow@hotmail.com>
2020-01-27 14:34:50 +0000

    Finished

---
+++ build/CMakeFiles/myscript.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myscript.dir/script/myscript.cpp.o"
+  "libmyscript.pdb"
+  "libmyscript.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myscript.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
